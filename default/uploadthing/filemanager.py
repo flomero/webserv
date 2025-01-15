7bd87f3bf34bc3f6928e7279e93243f7d478ab9f
flomero <49640251+flomero@users.noreply.github.com>
2025-01-15 18:43:01 +0100

    improve upload example

--- default/uploadthing/filemanager.py
+++ default/uploadthing/filemanager.py
@@ -6,25 +6,16 @@ import cgitb
 import json
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-# Enable error logging
 cgitb.enable()
 
-# Directory to manage
-# TODO: Change when CGI is fixed
-UPLOAD_DIR = "./default/uploadthing/uploads"
+UPLOAD_DIR = "./uploads"
 
 def list_files():
     return os.listdir(UPLOAD_DIR)
 
 def main():
     print("Content-Type: application/json\n")
-    
-    # Handle HTTP requests
-    form = cgi.FieldStorage()
-    request_method = os.environ.get('REQUEST_METHOD', 'GET')
-  
     files = list_files()
-    # remove .gitignore
     files = [f for f in files if f != ".gitignore"]
     print(json.dumps(files))
 
